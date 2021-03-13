72229d4dc7ed06b7e9bc5b139903b148cfee8f98
Anton <anton.butkievic@gmail.com>
2021-03-13 19:07:31 +0100

    deleted comments

--- DataBase.cpp
+++ DataBase.cpp
@@ -4,8 +4,7 @@ DataBase::DataBase(string c_OrdersFilePath, string c_OrderFilesFilePath, string 
 	: OrdersFilePath(c_OrdersFilePath)
 	, OrderItemsFilePath(c_OrderFilesFilePath)
 	, ProductsFilePath(c_ProductsFilePath)
-{	//cout << OrdersFilePath <<" "<<OrderItemsFilePath << " " << ProductsFilePath ;
-}
+{}
 
 
 void DataBase::MostProfitableProducts()
